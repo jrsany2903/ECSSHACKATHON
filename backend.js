4442731845815d8d4c29dc49f849ec9cb9c2600f
Joseph Arsany <jrsany2903@gmail.com>
2025-09-27 12:15:15 +0100

    added color password and validation, added skeleton for file saving

--- backend.js
+++ backend.js
@@ -1,33 +1,57 @@
 
 function validatePassword(pword){
+
+    console.log(pword)
+    const r = parseInt(pword.slice(1, 3), 16); // "3a"
+    const g = parseInt(pword.slice(3, 5), 16); // "7b"
+    const b = parseInt(pword.slice(5, 7), 16); // "d5"
     Errors = []
-    if (pword.includes("a")){
-        Errors.push("Sorry the letter \"a\" is out of commision today.")
+
+    if (r > 200){
+        Errors.push("Thats faar too much red.")
     }
 
-    if (pword.includes("k")){
-        Errors.push("Sorry the letter \"k\" is out of commision today.")
-    }
-    if (pword.includes("e")){
-        Errors.push("Sorry the letter \"e\" is out of commision today.")
+    if (g > 200){
+        Errors.push("Way too green.")
     }
 
-    if (pword.length < 9){
-        Errors.push("Password is too short, please make it at least 9 characters.")
+    if (b > 200){
+        Errors.push("I simply don't care for blue.")
     }
-    else if(pword.length < 12){
-        Errors.push("Just a little longer please :).")
 
-    }else if(pword.length < 15){
-        Errors.push("Almost there, just a little longer!")
-
-    }else if(pword.length < 17){
-        Errors.push("You can do it! Just a liiiitle longer.")
-
-    }else if(pword.length > 20){
-        Errors.push("Woaaah woaaah, way too long buddy.")
+    if (r + g + b < 250){
+        Errors.push("This colour is too dull.")
 
     }
+
+    // if (pword.includes("a")){
+    //     Errors.push("Sorry the letter \"a\" is out of commision today.")
+    // }
+
+    // if (pword.includes("k")){
+    //     Errors.push("Sorry the letter \"k\" is out of commision today.")
+    // }
+    // if (pword.includes("e")){
+    //     Errors.push("Sorry the letter \"e\" is out of commision today.")
+    // }
+
+    // if (pword.length < 9){
+    //     Errors.push("Password is too short, please make it at least 9 characters.")
+    // }
+    // else if(pword.length < 12){
+    //     Errors.push("Just a little longer please :).")
+
+    // }else if(pword.length < 15){
+    //     Errors.push("Almost there, just a little longer!")
+
+    // }else if(pword.length < 17){
+    //     Errors.push("You can do it! Just a liiiitle longer.")
+
+    // }else if(pword.length > 20){
+    //     Errors.push("Woaaah woaaah, way too long buddy.")
+
+    //}
+    
     
 
     console.log(Errors)
