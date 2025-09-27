5998bf8758102990686416d1a76f751a17c2a21f
Wiggledworm <williamcarrad@icloud.com>
2025-09-27 12:58:32 +0100

    translation of one input works now

--- storage.py
+++ storage.py
@@ -4,11 +4,14 @@ from googletrans import Translator, constants
 from pprint import pprint
 from flask import Flask, request, jsonify
 import asyncio
+from flask_cors import CORS
+
 
 
 ### CONNECTION ###
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/translate', methods=['POST'])
 ### translation
@@ -18,7 +21,7 @@ async def translate():
     # translates to arabic
     translator = Translator()
     y = await translator.translate(text, src="en",dest="ar")
-    return jsonify({'translated': y})
+    return jsonify({'translated': y.text})
 
 
 if __name__ == "__main__":
