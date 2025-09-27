0cc6477a8beb8bec2c2bddafd9950a0d61c1d6b5
U-SOTON\mc2g25 <chalkytangfastics@gmail.com>
2025-09-27 15:13:23 +0100

    Added placeholder validation, alerts are temp

--- backend.js
+++ backend.js
@@ -129,6 +129,9 @@ function Savetofile(forminfo){
 
 function SubmitButtonClick(){
     // Run whatever validation is needed beforehand
+    if (!validateBeforeSubmit()){
+        return
+    }
 
     // Open roulette Wheel modal
     document.getElementById("modal-roulette").style.display = "block"
@@ -252,9 +255,30 @@ function checkName() {
         document.getElementById("firstname").value = "NAMES DO NOT MATCH!";
         document.getElementById("surname").value = "DO YOU NOT KNOW YOR NAME!";
         document.getElementById("full").value = "I EXPECT BETTER FROM YOU!";
+        return false;
+    } else {
+        return true;
     }
 }
 
-function validateBeforeSubmit() {}
-function processSuccessfulSubmission() {}
-function processUnsuccessfulSubmission() {}
+function validateBeforeSubmit() {
+    let namesValid = checkName();
+    let passwordValid = validatePassword(document.getElementById("password").value);
+
+    if (!namesValid) {
+        alert("Names do not match!");
+    }
+
+    if (!passwordValid) {
+        alert("Password does not meet the criteria!");
+    }
+    return namesValid && passwordValid;
+}
+
+function processSuccessfulSubmission() {
+    alert("Submission successful!");
+}
+
+function processUnsuccessfulSubmission() {
+    alert("Submission unsuccessful!");
+}
